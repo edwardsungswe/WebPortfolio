b9b0ef1a29d0198fd639260b720a6b63ecc47cb4
edwardsung4217 <edwardsung4217@gmail.com>
2025-11-21 10:17:07 -0800

    Updated University

--- src/data/careerHistory.tsx
+++ src/data/careerHistory.tsx
@@ -2,6 +2,14 @@ let resumeData = {
   jobs: [
     {
       expData: {
+        company: "Master of Science in Computer Science",
+        title: "University of Southern California",
+        period: "June 2025 - Present",
+        details: [],
+      },
+    },
+    {
+      expData: {
         company: "FAST Enterprises",
         title: "Software Engineer",
         period: "June 2023 - present",
@@ -18,6 +26,14 @@ let resumeData = {
         details: [
           "At Clark County Water Reclamation District, I designed and launched the company’s first mobile application, improving operational efficiency for over 400 internal users. I developed an automated database rollback system that reduced deployment failure recovery time by 40%, ensuring smooth application updates. I also optimized database queries, reducing retrieval latency by 28%, and built a REST API to support the mobile app, enabling secure CRUD operations on internal employee databases.",
         ],
+      },
+    },
+    {
+      expData: {
+        company: "Bachelors of Science in Computer Science",
+        title: "University of Nevada, Las Vegas",
+        period: "August 2020 - May 2023",
+        details: [],
       },
     },
   ],
